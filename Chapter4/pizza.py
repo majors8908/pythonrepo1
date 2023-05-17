6695e90549fad2f92aef87554ef763572934b88f
majors8908 <matthew.s.majors@gmail.com>
2023-05-17 16:05:01 -0400

    startedchapter3

---
+++ Chapter4/pizza.py
@@ -0,0 +1,5 @@
+pizza_types = ['sausage', 'pepperoni', 'onions']
+for types in pizza_types:
+    print(f"Boy howdy do I love {types} on my pizza\n")
+
+print("well yes indeedy do i love me some peeza")
